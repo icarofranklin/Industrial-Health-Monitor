ce9445b44f51186e6ad7d201af575f5f7d4ead65
Icaro <icarofranklins@gmail.com>
2026-02-24 08:41:08 -0300

    Docker
    Aprendendo como usar Docker, primeiro uso da ferramenta

--- demo/src/main/java/com/example/demo/TelemetriaController.java
+++ demo/src/main/java/com/example/demo/TelemetriaController.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
-package com.example.demo;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/telemetria") // O endereço base
@@ -14,11 +15,11 @@ public class TelemetriaController {
 
     @PostMapping
     public void receberDados(@RequestBody Telemetria dados) {
-        // 1. O @RequestBody pega o JSON do Python e transforma no objeto 'Telemetria'
+        //O @RequestBody pega o JSON do Python e transforma no objeto 'Telemetria'
         
         System.out.println("Recebido: " + dados.getIdMaquina() + " | Temp: " + dados.getTemperatura());
 
-        // 2. Aqui vamos salvar no banco (Por enquanto, vai dar erro se rodar pois não temos banco configurado)
+        //Salvar no banco (Por enquanto, vai dar erro se rodar pois não temos banco configurado)
         telemetriaRepository.save(dados);
     }
 }
